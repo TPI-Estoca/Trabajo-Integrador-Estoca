671d6943661f8ce96cd9c34d6f0ea066f153079e
Fjmoya-uba <fjmoya@fi.uba.ar>
2025-11-20 18:49:30 -0300

    Reordeno

--- Ejercicio3/ScriptsEj3/tpi.py
+++ Ejercicio3/ScriptsEj3/tpi.py
@@ -3,13 +3,40 @@ import matplotlib.pyplot as plt
 import scipy.signal as signal
 import scipy.io.wavfile as wav  
 from IPython.display import Audio # reproducir en colab
+from scipy.linalg import toeplitz
 
 # Funciones auxiliares
 # ********************
 
-def param_lpc(xs, P):
-    # completar
-
+def param_lpc(xs, P): ## EMI, ULI, esto lo saqué del ejercicio 1a.
+    """
+    Calcula los parámetros LPC de un segmento de señal.
+    xs: segmento de señal (ya ventaneado)
+    P: orden del predictor (cantidad de coeficientes)
+    Retorna: (a, G) donde a son los coeficientes LPC y G es la ganancia
+    """
+    xs = np.asarray(xs, dtype=float)
+    N = len(xs)
+    
+    # Autocorrelación no sesgada para lags 0 a P
+    z = np.array([np.dot(xs[:N-k], xs[k:]) / (N-k) for k in range(P+1)])
+    
+    # Matriz de Toeplitz R (P x P) y vector r (P x 1)
+    R = toeplitz(z[:P])
+    r = z[1:P+1]
+    
+    # Resolver R*a = r (ecuaciones de Yule-Walker)
+    try:
+        a = np.linalg.solve(R, r)
+    except np.linalg.LinAlgError:
+        # Si R es singular, usar mínimos cuadrados
+        a = np.linalg.lstsq(R, r, rcond=None)[0]
+    
+    # Ganancia: G² = z[0] - a'*r
+    G2 = z[0] - np.dot(a, r)
+    G = np.sqrt(max(G2, 1e-12))  # Evitar raíz de negativos por errores numéricos
+    
+    return a, G
 
 def pitch_lpc(xs, a, alpha, fs):
     # completar
